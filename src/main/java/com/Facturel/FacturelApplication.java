b39e00634a180f1253018310f42e4338af85a40a
Gheorghe Coanta <gheorghe.coanta@gmail.com>
2021-06-27 19:48:36 +0300

    Init project Facturel

---
+++ src/main/java/com/Facturel/FacturelApplication.java
@@ -0,0 +1,13 @@
+package com.Facturel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Facturel/FacturelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Facturel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
